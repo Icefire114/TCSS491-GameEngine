5481ff6846410de80b394c245633cc791ebb5d3a
Icefire113 <phelan.gormley@gmail.com>
2026-01-21 22:28:48 -0800

    Update Entity.ts

--- src/engine/Entity.ts
+++ src/engine/Entity.ts
@@ -1,19 +1,19 @@
-import { ImagePath } from "./assetmanager";
-import { GameEngine } from "./gameengine";
-import { Collider } from "./physics/Collider";
-import { Vec2 } from "./types";
+import { ImagePath } from "./assetmanager.js";
+import { GameEngine } from "./gameengine.js";
+import { Collider } from "./physics/Collider.js";
+import { Vec2 } from "./types.js";
 
 /**
  * This is the parent type for all entities, and they should all extend this type.
  */
-
 export type Entity = {
+    readonly tag: string;
+
     position: Vec2;
     velocity: Vec2;
 
     physicsCollider: Collider | null;
     sprite: ImagePath | null;
-    readonly tag: string;
 
     removeFromWorld: boolean;
     draw(ctx: CanvasRenderingContext2D, game: GameEngine): void;
