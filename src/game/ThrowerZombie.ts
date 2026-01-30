39bb1fdaa8ee3049098f017c3404f421e6c5df43
Icefire113 <phelan.gormley@gmail.com>
2026-01-29 22:39:37 -0800

    Reminder of TODO for the zombie rotations

--- src/game/ThrowerZombie.ts
+++ src/game/ThrowerZombie.ts
@@ -82,6 +82,7 @@ export class ThrowerZombie implements Entity {
     }
 
     draw(ctx: CanvasRenderingContext2D, game: GameEngine): void {
+        // TODO: if moving rotate sprite to be perpidicular to the normal of the mountain slope
         this.animator.drawCurrentAnimFrameAtPos(ctx, this.position);
     }
 
