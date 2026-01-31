774ede4dd3da81353f3f3ed65fdf5ec71ce57504
Icefire113 <phelan.gormley@gmail.com>
2026-01-30 23:41:04 -0800

    Well that broke some shit

--- src/engine/types.ts
+++ src/engine/types.ts
@@ -41,7 +41,7 @@ export class Vec2 {
     x: number;
     y: number;
 
-    constructor(x: number, y: number) {
+    constructor(x: number = 0, y: number = 0) {
         this.x = x;
         this.y = y;
     }
