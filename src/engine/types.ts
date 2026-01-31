36b20074cc7a843f02d0e7003a563ac8287d8b6b
Icefire113 <phelan.gormley@gmail.com>
2026-01-30 23:39:07 -0800

    Vec2 constructor

--- src/engine/types.ts
+++ src/engine/types.ts
@@ -38,8 +38,13 @@ export namespace ResourcePath {
  * A 2D vector
  */
 export class Vec2 {
-    x: number = 0;
-    y: number = 0;
+    x: number;
+    y: number;
+
+    constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
 }
 
 export namespace Vec2 {
