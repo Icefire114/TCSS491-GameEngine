919d0bc49808f82e586cc0659a3c4b5016584180
Icefire113 <phelan.gormley@gmail.com>
2026-02-12 12:12:53 -0800

    Trigger to tell when player enters safe zone

---
+++ src/game/Triggers/BoxTrigger.ts
@@ -0,0 +1,54 @@
+import { ImagePath } from "../../engine/assetmanager.js";
+import { Entity, EntityID } from "../../engine/Entity.js";
+import { GameEngine } from "../../engine/gameengine.js";
+import { BoxCollider } from "../../engine/physics/BoxCollider.js";
+import { Collidable, Collider } from "../../engine/physics/Collider.js";
+import { Vec2 } from "../../engine/types.js";
+
+export class BoxTrigger implements Entity, Collidable {
+    tag: string = "BoxTrigger";
+    id: EntityID;
+    position: Vec2;
+    velocity: Vec2 = new Vec2();
+    physicsCollider: BoxCollider;
+    sprite: ImagePath | null = null;
+    removeFromWorld: boolean = false;
+    oneShot: boolean;
+    onTrigger: (other: Entity) => void;
+    triggerOnTags: string[];
+
+    /**
+     * 
+     * @param pos The position
+     * @param size The size of the trigger
+     * @param toTriggerOnTag A list of Entity tags that we should call `onTrigger` when we collide with.
+     * @param oneShot Should this trigger only once ever?
+     * @param onTrigger To call when we trigger.
+     */
+    constructor(pos: Vec2, size: Vec2, toTriggerOnTag: string[], oneShot: boolean, onTrigger: (other: Entity) => void) {
+        this.id = `${this.tag}#${crypto.randomUUID()}`;
+        this.position = pos;
+        this.physicsCollider = new BoxCollider(size.x, size.y);
+        this.oneShot = oneShot;
+        this.triggerOnTags = toTriggerOnTag;
+        this.onTrigger = onTrigger;
+    }
+
+    draw(ctx: CanvasRenderingContext2D, game: GameEngine): void {
+
+    }
+
+    update(keys: { [key: string]: boolean; }, deltaTime: number, clickCoords: Vec2): void {
+        for (const tag of this.triggerOnTags) {
+            const ents = GameEngine.g_INSTANCE.getEntitiesByTag(tag);
+            for (const ent of ents) {
+                if (this.physicsCollider.collides(this, ent)) {
+                    this.onTrigger(ent);
+                    if (this.oneShot) {
+                        this.removeFromWorld = true;
+                    }
+                }
+            }
+        }
+    }
+}
--- src/game/worldEntities/SafeZone.ts
+++ src/game/worldEntities/SafeZone.ts
@@ -1,8 +1,10 @@
+import { Animator } from "../../engine/Animator.js";
 import { ImagePath } from "../../engine/assetmanager.js";
 import { Entity, EntityID } from "../../engine/Entity.js";
 import { GameEngine } from "../../engine/gameengine.js";
 import { Collider } from "../../engine/physics/Collider.js";
-import { Vec2 } from "../../engine/types.js";
+import { DrawLayer, Vec2 } from "../../engine/types.js";
+import { BoxTrigger } from "../Triggers/BoxTrigger.js";
 
 export class SafeZone implements Entity {
     id: EntityID;
@@ -12,16 +14,29 @@ export class SafeZone implements Entity {
     physicsCollider: Collider | null = null;
     sprite: ImagePath | null = null;
     removeFromWorld: boolean = false;
+    // List of the shops/ things we will draw in the safe zone
+    animators: Animator[] = [];
+
+    readonly size: Vec2 = new Vec2(150, 50);
 
     constructor(pos: Vec2) {
         this.id = `${this.tag}#${crypto.randomUUID()}`;
         this.position = pos;
-
+        GameEngine.g_INSTANCE.addEntity(
+            new BoxTrigger(
+                Vec2.compAdd(this.position, new Vec2(5, 0)),
+                new Vec2(1, 100),
+                ["player"],
+                false,
+                (ent: Entity) => {
+                    console.log("Player entered the SafeZone!");
+                }
+            ), DrawLayer.DEFAULT
+        );
     }
 
     draw(ctx: CanvasRenderingContext2D, game: GameEngine): void {
-        // const canvasPos: Vec2 = GameEngine.g_INSTANCE.renderer.convertWorldPosToScreenPos(this.position, new Vec2(150, 100));
-        GameEngine.g_INSTANCE.renderer.drawRectAtWorldPos(this.position, new Vec2(150, 50), "rgba(0,0,0,0)", "#FF0000", 2);
+        GameEngine.g_INSTANCE.renderer.drawRectAtWorldPos(this.position, this.size, "rgba(0,0,0,0)", "#FF0000", 2);
     }
 
 
