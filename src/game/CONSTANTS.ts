562b367c920a552536a0101a3bf065bbe5ca7958
Icefire113 <phelan.gormley@gmail.com>
2026-02-15 22:07:50 -0800

    Disable cheats/ dev options in prod

--- src/game/CONSTANTS.ts
+++ src/game/CONSTANTS.ts
@@ -3,17 +3,17 @@
  */
 export const G_CONFIG = {
     // If true, draw boxes around the physics colliders
-    DRAW_PHYSICS_COLLIDERS: true,
+    DRAW_PHYSICS_COLLIDERS: false,
     // If true, draw the anchor points for the terrain
     DRAW_TERRAIN_ANCHOR_POINTS: false,
     // If true, don't generate terrain just a flat plane
     TERRAIN_GENERATION_FORCE_FLAT: false,
     // If true, player does not take damage
-    GOD_MODE: true,
+    GOD_MODE: false,
     // If true, use the new renderer, else use the old renderer
     NEW_RENDERER: false,
     // If true, draw the safezone bounding boxes
-    DRAW_SAFEZONE_BB: true,
+    DRAW_SAFEZONE_BB: false,
     // If true, create the entities right in front of the player
     CREATE_TESTING_ENTS: false
 }
