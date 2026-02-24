d028cb9cb161d7f74d4e104c142b3bfe24375ae5
Icefire113 <phelan.gormley@gmail.com>
2026-02-23 16:17:55 -0800

    debug things

--- src/game/CONSTANTS.ts
+++ src/game/CONSTANTS.ts
@@ -3,19 +3,19 @@
  */
 export const G_CONFIG = {
     // If true, draw boxes around the physics colliders
-    DRAW_PHYSICS_COLLIDERS: false,
+    DRAW_PHYSICS_COLLIDERS: true,
     // If true, draw the anchor points for the terrain
     DRAW_TERRAIN_ANCHOR_POINTS: false,
     // If true, don't generate terrain just a flat plane
     TERRAIN_GENERATION_FORCE_FLAT: false,
     // If true, player does not take damage
-    GOD_MODE: false,
+    GOD_MODE: true,
     // If true, draw the safezone bounding boxes
     DRAW_SAFEZONE_BB: false,
     // If true, create the entities right in front of the player
     CREATE_TESTING_ENTS: false,
     // If true, enable cheats by pressing 'f' for free fly and 'k' to force death
-    ENABLE_DEBUG_KEYS: false,
+    ENABLE_DEBUG_KEYS: true,
     // If true, player can access armory at anytime by pressing 'p', and can access all guns
-    UNLOCK_ALL_GUNS: false
+    UNLOCK_ALL_GUNS: true
 }
