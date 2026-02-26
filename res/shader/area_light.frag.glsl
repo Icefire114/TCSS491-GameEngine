2a96d0e1457f1171e6e26c6f3b29cd64814920cc
Icefire113 <phelan.gormley@gmail.com>
2026-02-25 23:51:53 -0800

    Revert "working shader slop"
    This reverts commit ec88c33a8d6b7970ed028f01b917d18646f44dbe.

--- res/shader/area_light.frag.glsl
+++ res/shader/area_light.frag.glsl
@@ -1,4 +1,6 @@
 #version 300 es
+#define MAX_LIGHTS 8
+
 precision highp float;
 
 in vec2 v_texCoord;
@@ -6,39 +8,42 @@ out vec4 fragColor;
 
 uniform sampler2D u_texture;
 
+uniform int u_lightCount;
 /// The ambient light 0-1
 uniform float u_ambient;
 /// In pixels
-uniform float u_lightSize;
+uniform float u_lightSize[MAX_LIGHTS];
 /// In pixels relative to the texture size, +x -> right +y -> up
-uniform vec2 u_lightPos;
+uniform vec2 u_lightPos[MAX_LIGHTS];
 /// rgb = color, a = intensity
-uniform vec4 u_lightColor;
+uniform vec4 u_lightColor[MAX_LIGHTS];
 
 void main() {
     vec4 texColor = texture(u_texture, v_texCoord);
 
-    // Early out for transparent pixels
     if(texColor.a < 0.01f) {
         fragColor = texColor;
         return;
     }
 
     vec2 px = gl_FragCoord.xy;
-    float dist = distance(px, u_lightPos);
 
-    float radius = max(u_lightSize, 1.0f);
+    vec3 totalLight = vec3(u_ambient);
+    for(int i = 0; i < MAX_LIGHTS; i++) {
+        if(i >= u_lightCount)
+            break;
 
-    // Smooth radial falloff
-    float falloff = 1.0f - smoothstep(0.0f, radius, dist);
+        float radius = max(u_lightSize[i], 1.0f);
+        float dist = distance(px, u_lightPos[i]);
 
-    // Softer inverse-square style attenuation
-    float atten = 1.0f / (1.0f + (dist * dist) / (radius * radius));
+        float falloff = 1.0f - smoothstep(0.0f, radius, dist);
+        float atten = 1.0f / (1.0f + (dist * dist) / (radius * radius));
 
-    float intensity = falloff * atten * u_lightColor.a;
+        float intensity = falloff * atten * u_lightColor[i].a;
 
-    vec3 light = u_lightColor.rgb * intensity;
-    vec3 finalColor = texColor.rgb * (u_ambient + light);
+        totalLight += u_lightColor[i].rgb * intensity;
+    }
 
+    vec3 finalColor = texColor.rgb * totalLight;
     fragColor = vec4(finalColor, texColor.a);
 }
